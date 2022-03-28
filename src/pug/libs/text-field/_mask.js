7ef15370abde3595787bc20734a19465d1af2a62
Nana <nanalit16@gmail.com>
2022-03-28 16:51:41 +0300

    маска ввода готова

--- src/pug/libs/text-field/_mask.js
+++ src/pug/libs/text-field/_mask.js
@@ -13,40 +13,49 @@ function maskedInput() {
     }
     if (+inputNumbersValue[0] <= 3) {
       formattedValue = `${inputNumbersValue[0]}`;
-      if ((inputNumbersValue.length >= 2 && +inputNumbersValue[0] <= 2) || (inputNumbersValue.length >= 2 && +inputNumbersValue[0] === 3 && (inputNumbersValue[1] === "0" || +inputNumbersValue[1] === 1))) {
+      if (inputNumbersValue.length >= 2) {
         formattedValue += inputNumbersValue[1]+".";
-        console.log(formattedValue);
-        if(inputNumbersValue.length >= 3 && +formattedValue <= 31) {
-          formattedValue += inputNumbersValue[2];
-          console.log(formattedValue);
-          if((inputNumbersValue.length >= 4 && inputNumbersValue[2] === "0") || (inputNumbersValue.length >= 4 && inputNumbersValue[2] === "1" && (inputNumbersValue[3] === "0" || inputNumbersValue[3] === "1" || inputNumbersValue[3] === "2"))) {
-            formattedValue += inputNumbersValue[3]+".";
-            console.log(formattedValue);
-            if(inputNumbersValue.length >= 5 && +inputNumbersValue[4] === 1) {
-              formattedValue += inputNumbersValue[4];
-              console.log(formattedValue);
-              if(inputNumbersValue.length >= 6 && +inputNumbersValue[5] === 9) {
-                formattedValue += inputNumbersValue[5];
+        if (+formattedValue.substring(0,2) <= 0 || +formattedValue.substring(0,2) > 31) {
+            formattedValue = "Введите корректное значение дня";
+          } else {
+            if(inputNumbersValue.length >= 3) {
+              formattedValue += inputNumbersValue[2];
+              if(inputNumbersValue.length >= 4) {
+                formattedValue += inputNumbersValue[3]+".";
                 console.log(formattedValue);
-                if(inputNumbersValue.length >= 7) {
-                  formattedValue += inputNumbersValue[6];
-                  console.log(formattedValue);
-                  if(inputNumbersValue.length >= 8) {
-                    formattedValue += inputNumbersValue[7];
-                    console.log(formattedValue);
+                if (+formattedValue.substring(3,5) <= 0 || +formattedValue.substring(3,5) > 12) {
+                    formattedValue = "Введите корректное значение месяца";
+                  } else {
+                    if(inputNumbersValue.length >= 5) {
+                      formattedValue += inputNumbersValue[4];
+                      console.log(formattedValue);
+                      if(inputNumbersValue.length >= 6) {
+                        formattedValue += inputNumbersValue[5];
+                        console.log(formattedValue);
+                        if(inputNumbersValue.length >= 7) {
+                          formattedValue += inputNumbersValue[6];
+                          console.log(formattedValue);
+                          if(inputNumbersValue.length >= 8) {
+                            formattedValue += inputNumbersValue[7];
+                            console.log(formattedValue);
+                            if (+formattedValue.substring(6,10) <= 1945 || +formattedValue.substring(6,10) > 2022) {
+                                formattedValue = "Введите корректное значение года рождения";
+                            }
+                          }
+                        }
+                      }
+                    }
                   }
-                }
-              }
+                
             }
           }
+            
         }
-        
+
       }
+        
     } else {
       formattedValue = "";
-    }
-    if (+formattedValue.substring(0,2) <= 0 || +formattedValue.substring(0,2) > 31) {
-      formattedValue = "Введите корректное значение";
     }
     input.value = formattedValue;
   };
