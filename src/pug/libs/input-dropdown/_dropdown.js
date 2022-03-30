712aadc98e650aba0a1a4ccd599be65e9659a7be
Nana <nanalit16@gmail.com>
2022-03-30 18:49:00 +0300

    Сверстала дропдаун

---
+++ src/pug/libs/input-dropdown/_dropdown.js
@@ -0,0 +1,9 @@
+function openList() {
+  let dropdownInput = document.querySelectorAll('.dropdown__title');
+  console.log(dropdownInput);
+  dropdownInput.addEventListener('clik', (e) => {
+    
+  });
+}
+
+module.exports = openList;
--- src/user.js
+++ src/user.js
@@ -1,7 +1,9 @@
 
 
 import maskedInput from "./pug/libs/text-field/_mask";
+import openList from "./pug/libs/input-dropdown/_dropdown";
 
 window.addEventListener('DOMContentLoaded', function() {
   maskedInput();
+  openList();
 });
