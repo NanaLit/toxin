1f57ea700ab9159af46315d7b7caf1c0349dcea3
Nana <nanalit16@gmail.com>
2022-04-03 15:51:20 +0300

    работают счетчики в дропдауне

--- src/pug/libs/input-dropdown/_dropdown.js
+++ src/pug/libs/input-dropdown/_dropdown.js
@@ -1,12 +1,40 @@
 
 function dropdown() {
   let dropdownInput = document.querySelectorAll('.dropdown__title'),
-      dropdownContent = document.querySelectorAll('.dropdown-wrapper');
-      
+      dropdownContent = document.querySelectorAll('.dropdown-wrapper'),
+      plus = document.querySelectorAll('.plus'),
+      minus = document.querySelectorAll('.minus'),
+      counter = document.querySelectorAll('.counter__number');
   dropdownInput.forEach((item, i, arr) => {
     arr[i].addEventListener('click', () => {
       item.classList.toggle('dropdown__title_active');
       dropdownContent[i].classList.toggle('hide');
+    });
+  });
+  
+  plus.forEach((item, i, arr) => {
+    let count = 0;
+      arr[i].addEventListener('click', () => {
+        count = ++count;
+        if (count <= 5) {
+        counter[i].innerHTML = count;
+      } else {
+        count = 0;
+      }
+    });
+    
+  });
+  
+  minus.forEach((item, i, arr) => {
+    arr[i].addEventListener('click', () => {
+      let count = counter[i].innerHTML;
+      count = --count;
+      if (count >= 0) {
+        counter[i].innerHTML = count;
+      } else {
+        counter[i].innerHTML = 0;
+      }
+      
     });
   });
 }
