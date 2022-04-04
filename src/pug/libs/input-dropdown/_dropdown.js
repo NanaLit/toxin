3baf539bf38798301e47ab71d333ad7b8bae28d2
Nana <nanalit16@gmail.com>
2022-04-04 20:08:29 +0300

    в инпуты добавляетсяколичесво гостей

--- src/pug/libs/input-dropdown/_dropdown.js
+++ src/pug/libs/input-dropdown/_dropdown.js
@@ -1,10 +1,14 @@
 
 function dropdown() {
   let dropdownInput = document.querySelectorAll('.dropdown__title'),
+      dropdownInputGuest = document.querySelector('.dropdown__title_guests'),
       dropdownContent = document.querySelectorAll('.dropdown-wrapper'),
       plus = document.querySelectorAll('.plus'),
       minus = document.querySelectorAll('.minus'),
-      counter = document.querySelectorAll('.counter__number');
+      counter = document.querySelectorAll('.counter__number'),
+      guests = document.querySelectorAll('.guests>.counter__number'),
+      rooms = document.querySelectorAll('.rooms>.counter__number');
+      
   dropdownInput.forEach((item, i, arr) => {
     arr[i].addEventListener('click', () => {
       item.classList.toggle('dropdown__title_active');
@@ -14,15 +18,18 @@ function dropdown() {
   
   plus.forEach((item, i, arr) => {
     let count = 0;
-      arr[i].addEventListener('click', () => {
+      arr[i].addEventListener('click', function(event) {
+        const target = event.target;
         count = ++count;
         if (count <= 5) {
         counter[i].innerHTML = count;
+        if (target && target.classList.contains('guests')) {
+          dropdownInputGuest.innerHTML = `${+counter[1].innerHTML+(+counter[0].innerHTML)} гостя`;
+        }
       } else {
         count = 0;
       }
     });
-    
   });
   
   minus.forEach((item, i, arr) => {
@@ -34,8 +41,8 @@ function dropdown() {
       } else {
         counter[i].innerHTML = 0;
       }
-      
     });
   });
+  
 }
 module.exports = dropdown;
