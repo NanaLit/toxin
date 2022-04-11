6523a9e0910a5d4837778c46f03b7899fdbc1890
Nana <nanalit16@gmail.com>
2022-04-11 21:20:17 +0300

    дропдаун готов

--- src/pug/libs/input-dropdown/_dropdown.js
+++ src/pug/libs/input-dropdown/_dropdown.js
@@ -3,13 +3,13 @@ function dropdown() {
   let dropdownInput = document.querySelectorAll('.dropdown__title'),
       dropdownInputGuest = document.querySelector('.dropdown__title_guests'),
       dropdownContent = document.querySelectorAll('.dropdown-wrapper'),
-      plus = document.querySelectorAll('.plus'),
-      minus = document.querySelectorAll('.minus'),
-      counter = document.querySelectorAll('.counter__number'),
+      counterWrapper = document.querySelectorAll('.counter'),
+      counterText = document.querySelectorAll('.counter__number'),
+      count,
       clear = document.querySelector('.clear'),
       apply = document.querySelector('.apply'),
       guestName = 'гость',
-      littleGuest = 'младенец';
+      littleGuestName = 'младенец';
       
   dropdownInput.forEach((item, i, arr) => {
     arr[i].addEventListener('click', () => {
@@ -18,100 +18,109 @@ function dropdown() {
     });
   });
 
-  function namedGuests(numbers) {
-    if (numbers > 1 && numbers < 5) {
+  function namedGuests(numbers) { 
+    if (numbers === 1) {
+      guestName = 'гость';
+    } else if (numbers > 1 && numbers < 5) {
       guestName = 'гостя';
     } else if (numbers >= 5) {
       guestName = 'гостей';
     }
     return guestName;
   }
-  function namedGuestsMinus(numbers) {
-    if (numbers < 5 && numbers > 1) {
-      guestName = 'гостя';
-    } else if ( numbers === 1) {
-      guestName = 'гость';
-    }
-    return guestName;
-  }
 
   function namedLittleGuests(number) {
-    if (number > 1 && number < 5) {
-      littleGuest = 'младенца';
+    if (number === 1) {
+      littleGuestName = 'младенец';
+    } else if (number > 1 && number < 5) {
+      littleGuestName = 'младенца';
     } else if (number >= 5) {
-      littleGuest = 'младенцев';
+      littleGuestName = 'младенцев';
     }
-    return littleGuest;
+    return littleGuestName;
   }
 
-  function namedLittleGuestsMinus(number) {
-    if (number < 5 && number > 1) {
-      littleGuest = 'младенца';
-    } else if (number === 1) {
-      littleGuest = 'младенец';
-    }
-    return littleGuest;
-  }
-  
-  plus.forEach((item, i, arr) => {
-    let count = 0;
-    
-      arr[i].addEventListener('click', function(event) {
-        
-        const target = event.target;
-        count = ++count;
-        if (count !== 0) {
-          clear.classList.remove('hidden');
-        }
-        if (count <= 5) {
-        counter[i].innerHTML = count;
-        if (target && target.classList.contains('guests') && +counter[2].innerHTML === 0) {
-          namedGuests(+counter[1].innerHTML+(+counter[0].innerHTML));
-          dropdownInputGuest.innerHTML = `${+counter[1].innerHTML+(+counter[0].innerHTML)} ${guestName}`;
-        } else if (target && target.classList.contains('guests') && +counter[2].innerHTML > 0) {
-          namedLittleGuests(+counter[2].innerHTML);
-          dropdownInputGuest.innerHTML = `${+counter[1].innerHTML+(+counter[0].innerHTML)} ${guestName}, ${+counter[2].innerHTML} ${littleGuest}`;
-        }
-      } else {
-        count = 0;
-      }
-    });
-  });
-  
-  minus.forEach((item, i, arr) => {
-    arr[i].addEventListener('click', function(event) {
-      const target = event.target;
-      let count = counter[i].innerHTML;
-      count = --count;
-      if (count >= 0) {
-        counter[i].innerHTML = count;
-        if (target && target.classList.contains('guests') && +counter[2].innerHTML === 0) {
-          namedGuestsMinus(+counter[1].innerHTML+(+counter[0].innerHTML));
-          dropdownInputGuest.innerHTML = `${+counter[1].innerHTML+(+counter[0].innerHTML)} ${guestName}`; 
-        } else if (target && target.classList.contains('guests') && +counter[2].innerHTML > 0) {
-          namedLittleGuestsMinus(+counter[2].innerHTML);
-          dropdownInputGuest.innerHTML = `${+counter[1].innerHTML+(+counter[0].innerHTML)} ${guestName}, ${+counter[2].innerHTML} ${littleGuest}`; 
-        }
-      } else {
-        counter[i].innerHTML = 0;
-      }
-      if(+counter[0].innerHTML === 0 && +counter[1].innerHTML === 0) {
-        clear.classList.add('hidden');
-      }
-    });
-  });
-  
   function clearField() {
     clear.addEventListener('click', function(event) {
       const target = event.target;
       if (target && target.classList.contains('clear')) {
-        console.log('hi');
         dropdownInputGuest.innerHTML = `Сколько гостей`;
         clear.classList.add('hidden');
+        counterText.forEach(item => {
+          item.innerHTML = 0;
+        });
       }
     });
   }
   clearField();
+
+  
+
+  function hiddenClear() {
+    if (+counterText[1].innerHTML+(+counterText[0].innerHTML) !==0) {
+      clear.classList.remove('hidden');
+    }
+    if (+counterText[1].innerHTML+(+counterText[0].innerHTML) ===0) {
+      clear.classList.add('hidden');
+    }
+  }
+  
+  function getCounter(event) {
+
+    for (let i = 0; i < counterText.length; i++) {
+      if (counterWrapper[i] === event.currentTarget) {
+        count = Number(counterText[i].innerHTML);
+      }
+    }
+
+    if (event.target.closest(".minus")) {
+      count -= 1;
+      if (count < 0) {
+        count = 6;
+      }
+    }
+
+    if (event.target.closest(".plus")) {
+      count += 1;
+      if (count > 6) {
+        count = 0;
+      }
+    }
+    for (let i = 0; i < counterText.length; i++) {
+      if (counterWrapper[i] === event.currentTarget) {
+        counterText[i].innerHTML = count;
+        hiddenClear();
+      }
+    }
+    return count;
+  }
+
+  for (let i = 0; i < counterWrapper.length; i++) {
+    counterWrapper[i].addEventListener("click", getCounter);
+  }
+
+  function getGuests() {
+    counterWrapper.forEach((item, i, arr) => {
+      arr[i].addEventListener('click', function(event) {
+        const target = event.target;
+        let bigGuest = +counterText[1].innerHTML+(+counterText[0].innerHTML);
+        let littleGuest = +counterText[2].innerHTML;
+        if (target && target.classList.contains('guests') && littleGuest === 0 && bigGuest > 0) {
+          namedGuests(bigGuest);
+          dropdownInputGuest.innerHTML = `${bigGuest} ${guestName}`;
+        } else if (target && target.classList.contains('guests') && bigGuest > 0 && bigGuest > 0) {
+          namedLittleGuests(littleGuest);
+          namedGuests(bigGuest);
+          dropdownInputGuest.innerHTML = `${bigGuest} ${guestName}, ${littleGuest} ${littleGuestName}`;
+        } else if (target && target.classList.contains('guests') && bigGuest === 0) {
+          counterText[2].innerHTML = 0;
+          dropdownInputGuest.innerHTML = `Сколько гостей`;
+        }
+      });
+    });
+  }
+  getGuests();
+
   function applyField() {
     apply.addEventListener('click', function(event) {
       const target = event.target;
